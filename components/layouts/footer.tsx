960d5d7d6d33df79ebd13c97b2cca84644d25154
Wibumoe <131719695+wibumoe@users.noreply.github.com>
2024-03-05 01:53:07 +0700

    Update footer.tsx

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -71,18 +71,6 @@ export default function Footer() {
                             <span className="sr-only">{item.name}</span>
                             <item.icon className="h-6 w-6" aria-hidden="true" />
                         </a>
-            <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4842816,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4842816&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
                     ))}
                 </div>
                 <div className="mt-8 md:order-1 md:mt-0">
